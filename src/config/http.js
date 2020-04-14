3a29debd5fd764fb7d6d56323693e0c3be23abde
Jorge Vilca <jkodex@gmail.com>
2020-04-14 01:10:52 -0500

    deploy configuration to firebase

--- src/config/http.js
+++ src/config/http.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = {
+    'development': 'http://localhost:3000',
+    'staging': 'https://janu.yian.org.pe',
+    'production': 'https://api.janu.org.pe'
+}[process.env.NODE_ENV || 'development']
 
 export default axios
